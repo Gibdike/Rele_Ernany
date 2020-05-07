5f234b31841c6a0b4f04bff564568a3915c7406c
Gabriel Honda <vegancraft13@gmail.com>
2020-05-07 11:50:46 -0400

    Criação do esqueleto da HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +29,10 @@ class HomePage extends StatelessWidget {
   }
 
   _body() {
-    return Container();
+    return Container(
+      child: ListView(
+        children: <Widget>[],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
     );
